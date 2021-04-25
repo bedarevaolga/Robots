df6469943b1821d02d30c4e6c84c506a4b6676e5
bedarevaolga <olh4bedareva@yandex.by>
2021-04-25 16:24:47 +0300

    Robots

---
+++ src/PartsOfRobot.java
@@ -0,0 +1,12 @@
+public enum PartsOfRobot {
+    HEAD,
+    BODY,
+    RIGHT_FOOT,
+    LEFT_FOOT,
+    RIGHT_HAND,
+    LEFT_HAND;
+
+    public static PartsOfRobot getRandom() {
+        return values()[(int) (Math.random() * values().length)];
+    }
+}
